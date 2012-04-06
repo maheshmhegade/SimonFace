45dc3abe14b0611a2888873c2ca4b0a19cf39a2f
Mahesh <maheshmhegade@gmail.com>
2012-04-06 11:06:07 +0530

    minor edits

--- simonface/simonface.cpp
+++ simonface/simonface.cpp
@@ -18,7 +18,8 @@ void SimonFace::SimonFaceDetection(){
     CvHaarClassifierCascade *cascade;
     CvMemStorage            *storage;
     CvCapture *capture;
-    int       key,i;
+    int   i;
+    char  key;
     char      *filename = "/home/mmh/kde/src/SimonFace/simonface/data/haarcascade_frontalface_alt.xml";
     cascade = ( CvHaarClassifierCascade* )cvLoad( filename, 0, 0, 0 );
     storage = cvCreateMemStorage( 0 );
@@ -34,31 +35,19 @@ void SimonFace::SimonFaceDetection(){
         cvRectangle( frame,cvPoint( r->x, r->y ),cvPoint( r->x + r->width, r->y + r->height ),CV_RGB( 0, 255, 0 ), 1, 8, 0 );
       }
     cvCvtColor(frame,frame,CV_BGR2RGB);
+    
     QImage image = QImage((unsigned char *)frame->imageDataOrigin,frame->width,frame->height,QImage::Format_RGB888);
-      
     QGraphicsScene* scene = new QGraphicsScene(this);
     QPixmap tempimage=QPixmap::fromImage(image);
     ui->graphicsView_2->resize(211, 161); 
-    
-    ui->graphicsView_2->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    ui->graphicsView_2->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-
     QPixmap *backgroundPixmap = &tempimage;
     QPixmap sized = backgroundPixmap->scaled(QSize(ui->graphicsView_2->width(),ui->graphicsView_2->height()),Qt::KeepAspectRatioByExpanding);
     QImage sizedImage = QImage(sized.toImage());
-    QImage sizedCroppedImage = QImage(sizedImage.copy(0,0,
-       (ui->graphicsView_2->width() - 1.5),
-       (ui->graphicsView_2->height() + 19))); 
-    
-    QGraphicsPixmapItem *sizedBackground = scene->addPixmap(
-    QPixmap::fromImage(sizedCroppedImage));
-    sizedBackground->setZValue(1);
+    QGraphicsPixmapItem *sizedBackground = scene->addPixmap(QPixmap::fromImage(sizedImage));
     ui->graphicsView_2->setScene(scene);
     key = cvWaitKey(1);
     }
-    
     cvReleaseCapture( &capture );
-    cvDestroyWindow( "video" );
     cvReleaseHaarClassifierCascade( &cascade );
     cvReleaseMemStorage( &storage );
  return;
